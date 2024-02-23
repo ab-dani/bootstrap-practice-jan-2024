3a6f570717c84cf7882b149aef4316d335e2210e
Antonio Brown <135006436+ab-dani@users.noreply.github.com>
2024-02-23 06:40:58 -0600

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,22 @@
+document.getElementById("goodButton").addEventListener("click", function () {
+  document.getElementById("goodbuttonRslt").style.display = "block";
+});
+
+document.getElementById("goodButton2").addEventListener("click", function () {
+  document.getElementById("goodButtonRslt2").style.display = "block";
+});
+
+document.getElementById("badButton").addEventListener("click", function () {
+  document.getElementById("badbuttonrslt").style.display = "block";
+});
+
+document.getElementById("badbutton2").addEventListener("click", function () {
+  document.getElementById("badbuttonRslt2").style.display = "block";
+});
+
+
+document.getElementById("fineButton").addEventListener("click", function(){
+
+
+  document.getElementById("fineButtonRslt").style.display = "block";
+});
